bc4709e68b1fd42b844c15e5354fc46a7c6e41bd
Paulo Alberto Sousa <p.sousa612@gmail.com>
2020-07-20 05:55:05 -0700

    Add theme selector and fix the no selection bug (hopefully)

--- Tipp/SettingsViewController.swift
+++ Tipp/SettingsViewController.swift
@@ -16,30 +16,111 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var seg1Field: UITextField!
     @IBOutlet weak var seg2Field: UITextField!
     
+    @IBOutlet weak var line: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Load in the selected theme, defaults to the green/yellow
+        applyTheme(bgColor: defaults.string(forKey: "bgColor") ?? "85a392", highlightColor: defaults.string(forKey: "highlightColor") ?? "f5b971")
+        
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //Set-up the percentage fields
         let segPercents = defaults.object(forKey: "segPercents") as? [Double]
         //Load values into the tip
         seg0Field.text = String(format: "%0.0f%%", segPercents![0]*100)
         seg1Field.text = String(format: "%0.0f%%", segPercents![1]*100)
         seg2Field.text = String(format: "%0.0f%%", segPercents![2]*100)
+        
+        
+        
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        //Grab our new percentage values from the 3 fields
+        let rawPercentages = [seg0Field.text, seg1Field.text, seg2Field.text];
+        var percentages : [Double] = []
+        
+        //Remove the % and scale down by 100 (/100)
+        for item in rawPercentages {
+            let item = String(item!.dropLast())
+            percentages.append((Double(item) ?? 0) / 100)
+            
+        }
+        
+        defaults.set(percentages, forKey: "segPercents")
+        defaults.synchronize();
+        
+    }
+
     
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  
+    @IBAction func percentageFieldEditingStart(_ sender: UITextField) {
+        //Remove the '%' to avoid logic errors later
+        sender.text = String(sender.text!.dropLast())
     }
-    */
-
+    
+    @IBAction func percentageFieldChanged(_ sender: UITextField) {
+        //Add the '%' back for clarity
+        sender.text = sender.text! + "%"
+    }
+    
+    //Theres def a better way to do this but i dont know it yet
+    @IBAction func button1Pressed(_ sender: Any) {
+        applyTheme(bgColor: "85a392", highlightColor: "f5b971")
+    }
+    
+    @IBAction func button2Pressed(_ sender: Any) {
+        applyTheme(bgColor: "dfd3c3", highlightColor: "596e79")
+    }
+    
+    @IBAction func button3Pressed(_ sender: Any) {
+        applyTheme(bgColor: "ffaaa5", highlightColor: "fcf8f3")
+    }
+    
+    @IBAction func button4Pressed(_ sender: Any) {
+        applyTheme(bgColor: "3f4d71", highlightColor: "ffac8e")
+    }
+    
+    func applyTheme(bgColor: String, highlightColor: String) {
+        let unwrappedBg = unwrapHex(hex: bgColor)
+        let unwrappedHighlight = unwrapHex(hex: highlightColor)
+        
+        view.backgroundColor = unwrappedBg
+        line.backgroundColor = unwrappedHighlight
+        
+        self.navigationController?.navigationBar.barTintColor = unwrappedHighlight
+        self.navigationController?.navigationBar.tintColor = unwrappedBg
+        
+        defaults.set(bgColor, forKey: "bgColor")
+        defaults.set(highlightColor, forKey: "highlightColor")
+        defaults.synchronize()
+    }
+    
+    //Convert a hex string to UIColor
+    //I found this online (ty stackoverflow)
+    func unwrapHex(hex: String) -> UIColor {
+        var hexInt:UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&hexInt)
+        
+        let _red = CGFloat((hexInt & 0xFF0000) >> 16) / 255.0
+        let _green = CGFloat((hexInt & 0x00FF00) >> 8) / 255.0
+        let _blue = CGFloat(hexInt & 0x0000FF) / 255.0
+        let _alpha = CGFloat(1.0)
+        
+        return UIColor(red: _red, green: _green, blue: _blue, alpha: _alpha)
+    }
 }
--- Tipp/ViewController.swift
+++ Tipp/ViewController.swift
@@ -18,69 +18,160 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var tipField: UITextField!
     
+    @IBOutlet weak var line1: UILabel!
+    @IBOutlet weak var line2: UILabel!
+    @IBOutlet weak var line3: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //Set up defaults if none exist
-
+        //Set up observer for when app goes to the background & when it comes back in
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(appMovedToBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+    }
+    
+    @objc func appMovedToBackground() {
+        defaults.set(NSDate(), forKey: "lastTime");
+    }
+    
+    @objc func appCameToForeground() {
+        handleBillLogic();
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Set up defaults if none exist -> I think optionals were made to avoid stuff like this.....
         if(!defaults.bool(forKey: "init")) {
-            print("not init")
+            //print("not init")
             defaults.set(true, forKey: "init")
             defaults.set([0.15, 0.18, 0.2], forKey: "segPercents")
             defaults.set(1, forKey: "selectedSeg")
             defaults.set("", forKey: "tipField")
             defaults.set("", forKey: "billField")
-//
+            defaults.set(NSDate(), forKey: "lastTime") //shouldnt be needed but its here for safety
             
         } else {
-            print("init")
+            //Load saved values
+            //print("init")
             defaults.set(true, forKey: "init")
             
             tipControl.selectedSegmentIndex = defaults.integer(forKey: "selectedSeg")
+            
+            //Refresh the segmented control with the stored values
+            let percentages = defaults.object(forKey: "segPercents") as? [Double]
+            tipControl.removeAllSegments()
+            
+            for i in 0...2 {
+                tipControl.insertSegment(withTitle: String(format: "%g%%",percentages![i] * 100), at: i, animated: true)
+            }
+            
             billField.text = defaults.string(forKey: "billField")
             tipField.text = defaults.string(forKey: "tipField")
             
             //logic for the greater than 10 mins will go here me thinks
+            handleBillLogic()
             
             let tipPercent : Double
             if(tipControl.selectedSegmentIndex != -1) {
-                tipPercent = (defaults.object(forKey: "segPercents") as? [Double])![tipControl.selectedSegmentIndex] / 100
+                tipPercent = (defaults.object(forKey: "segPercents") as? [Double])![tipControl.selectedSegmentIndex]
+            } else if(!(tipField.text?.isEmpty ?? true) ) {
+                print("asda")
+                tipPercent = Double((tipField.text!).dropLast()) ?? 0
+             
+                
             } else {
-                tipPercent = Double((tipField.text!).dropLast())! / 100
+                tipPercent = 0;
             }
             
-            calculateAndDisplayTip(tipPercentage: tipPercent)
+            calculateAndDisplayTip(tipPercentage: tipPercent / 100)
         }
         
         defaults.synchronize()
+
+        //Load in the selected theme
+        let bgColor = unwrapHex(hex: defaults.string(forKey: "bgColor") ?? "85a392")
+        let highlightColor = unwrapHex(hex: defaults.string(forKey: "highlightColor") ?? "f5b971")
+        
+        //Set main background color
+        view.backgroundColor = bgColor
+        self.navigationController?.navigationBar.barTintColor = highlightColor
+        self.navigationController?.navigationBar.tintColor = bgColor
+        
+        //Set highlight pieces
+        tipControl.backgroundColor = highlightColor
+        totalLabel.textColor = highlightColor
+        
+        line1.backgroundColor = highlightColor
+        line2.backgroundColor = highlightColor
+        line3.backgroundColor = highlightColor
+        
         
     }
+    
+    //Convert a hex string to UIColor
+    //I found this online (ty stackoverflow)
+    func unwrapHex(hex: String) -> UIColor {
+        var hexInt:UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&hexInt)
+        
+        let _red = CGFloat((hexInt & 0xFF0000) >> 16) / 255.0
+        let _green = CGFloat((hexInt & 0x00FF00) >> 8) / 255.0
+        let _blue = CGFloat(hexInt & 0x0000FF) / 255.0
+        let _alpha = CGFloat(1.0)
+        
+        return UIColor(red: _red, green: _green, blue: _blue, alpha: _alpha)
+    }
+    
+    //Check time since last used
+    func handleBillLogic() {
+        let lastTime = defaults.object(forKey: "lastTime") as! Date
+        let elapsed = Date().timeIntervalSince(lastTime)
+        print(elapsed)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        defaults.set(NSDate(), forKey: "lastTime");
+        print(NSDate());
+    }
+    
+    
 
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
     
+    //Code below is a little spaghetti and is worth refactor if it is to be taken any further
+    
+    @IBAction func tipFieldEditingStart(_ sender: UITextField) {
+        if(String(sender.text!).last == "%") {
+            sender.text = String(sender.text!.dropLast());
+        }
+    }
+    
     @IBAction func tipFieldEdited(_ sender: Any) {
          //Add the '%' suffix to the field for clairty
-         if(tipField.text! == "") {
-             return;
+        
+        if(tipField.text == "") {
+            return;
+        }
+        
+        if(tipField.text?.last != "%") {
+            tipField.text = tipField.text! + "%"
          }
-         
-         if(tipField.text!.last! != "%") {
-             tipField.text = tipField.text! + "%"
-         }
-         
-        //Store data
          
         //Call the helper function to calculate tip
         calculateTipFromField()
     }
     
+    //Triggered when billField or tipControl is modified
     @IBAction func calculateTip(_ sender: Any) {
         
+        //Detection for when the percentage was entered in from the field instead selected in tipControl
         if(tipControl.selectedSegmentIndex == -1) {
             calculateTipFromField()
             return;
@@ -89,6 +180,7 @@ class ViewController: UIViewController {
         //Clear the tip field for clarity
         tipField.text = ""
         
+        //
         let tipPercentages = defaults.object(forKey: "segPercents") as? [Double]
     
         calculateAndDisplayTip(tipPercentage: tipPercentages![tipControl.selectedSegmentIndex])
@@ -100,8 +192,8 @@ class ViewController: UIViewController {
         //Deselect the segemented control
         tipControl.selectedSegmentIndex = -1;
         
+        //Calculate, scale, and pass the tip percentage entered in from the field
         var tipPercent = Double((tipField.text!).dropLast()) ?? 0
-        
         tipPercent /= 100
         
         calculateAndDisplayTip(tipPercentage : tipPercent)
@@ -110,6 +202,7 @@ class ViewController: UIViewController {
     
     //tipPercentage should already be divided by 100
     func calculateAndDisplayTip(tipPercentage: Double) {
+        print("calculate")
          //Save the needed data
          defaults.set(tipControl.selectedSegmentIndex, forKey: "selectedSeg")
          defaults.set(billField.text, forKey: "billField")
